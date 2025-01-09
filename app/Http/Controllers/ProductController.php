e6c936dcab7790b9166421c1d3fb8eb34201cea6
Md. Kamrul Hasan <55704250+kamrul15-8841@users.noreply.github.com>
2025-01-09 15:28:35 +0600

    hint

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-                $products = Product::with(['size', 'color'])->get();
+        $products = Product::with(['size', 'color'])->get();
         return view('settings.product.index', compact('products'));
     }
 
